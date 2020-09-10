f65f1d601021977a68c0199734a87808f6e15b28
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-10 11:56:45 +0300

    adding deserializer

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -20,7 +20,7 @@ class BeerDtoTest extends BaseTest{
 
     @Test
     void testDeserialize() throws IOException {
-        String json = "{\"beerId\":\"978f0d76-3892-4626-b8c4-1ef5aaa1adb1\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":2134141244,\"price\":12.99,\"createdDate\":\"2020-09-10T10:53:25.6922521+03:00\",\"lastUpdatedDate\":\"2020-09-10T10:53:25.6926589+03:00\"}";
+        String json = "{\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":2134141244,\"price\":\"12.99\",\"createdDate\":\"2020-09-10T11:53:47+0300\",\"lastUpdatedDate\":\"2020-09-10T11:53:47.2662901+03:00\",\"myLocalDate\":\"20200910\",\"beerId\":\"3edf5cef-f28e-4777-b7b1-3f802e4793cf\"}";
 
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 
