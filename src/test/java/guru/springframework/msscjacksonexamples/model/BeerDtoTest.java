7dde10e89ad51ea8a8800dfc6975077f4f5b3f30
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-10 11:46:14 +0300

    adding different formats

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -20,7 +20,7 @@ class BeerDtoTest extends BaseTest{
 
     @Test
     void testDeserialize() throws IOException {
-        String json = "{\"id\":\"978f0d76-3892-4626-b8c4-1ef5aaa1adb1\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":2134141244,\"price\":12.99,\"createdDate\":\"2020-09-10T10:53:25.6922521+03:00\",\"lastUpdatedDate\":\"2020-09-10T10:53:25.6926589+03:00\"}";
+        String json = "{\"beerId\":\"978f0d76-3892-4626-b8c4-1ef5aaa1adb1\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":2134141244,\"price\":12.99,\"createdDate\":\"2020-09-10T10:53:25.6922521+03:00\",\"lastUpdatedDate\":\"2020-09-10T10:53:25.6926589+03:00\"}";
 
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 
